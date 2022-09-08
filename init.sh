d266b6d9b67695c972164f60e0500dec23acaaf3
P.J. Finlay <pjappdevelopment@gmail.com>
2022-09-08 18:24:44 -0500

    Run Argos Deploy

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ARGOS_GIT_DIR="$HOME/git"
+
+# Install git repositories
+for gitrepository in $(cat "git-repositories")
+do
+	cd $ARGOS_GIT_DIR
+	if [ ! -d $gitrepository ] ; then
+		git clone $gitrepository 2>/dev/null
+	fi
+done
+
+# Run Argos Deploy
+ARGOS_DEPLOY_DIR="$ARGOS_GIT_DIR/argos-deploy"
+cd $ARGOS_DEPLOY_DIR
+for s in $(ls s)
+do
+	. "s/$s/init.sh"
+done
+
